a89bd6d09a8d8fc1c667e19a3da4a78abe47f14b
naman42624 <75940862+naman42624@users.noreply.github.com>
2023-07-26 20:29:16 +0530

    first commit

---
+++ storefront/playground/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+# request handler -> takes request and returns response
+# action
+def say_hello(request):
